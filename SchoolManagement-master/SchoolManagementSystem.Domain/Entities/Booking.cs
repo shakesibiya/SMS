ce80471d3febe7d88ec0ffed93363178c0e44e78
Ndabe <ndabenhlelanga@gmail.com>
2016-10-10 21:17:41 +0200

    reverted Simphiwe changes

--- SchoolManagement-master/SchoolManagementSystem.Domain/Entities/Booking.cs
+++ SchoolManagement-master/SchoolManagementSystem.Domain/Entities/Booking.cs
@@ -16,8 +16,6 @@ namespace SchoolManagementSystem.Domain.Entities
         public string Email { get; set; }
         public string Status { get; set; }
 
-        public int MyProperty { get; set; }
-
         public virtual Event Event { get; set; }
     }
 }
