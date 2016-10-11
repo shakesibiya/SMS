4087c88dc3e884da5a18e89eebb43b2ca3d19dd1
Ndabe <ndabenhlelanga@gmail.com>
2016-10-11 04:32:24 +0200

    Booking & Cancellation completed

--- SchoolManagement-master/SchoolManagementSystem/Controllers/BookingController.cs
+++ SchoolManagement-master/SchoolManagementSystem/Controllers/BookingController.cs
@@ -3,6 +3,9 @@ using SchoolManagementSystem.Domain.Entities;
 using SchoolManagementSystem.Models;
 using System.Data.Entity;
 using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace SchoolManagementSystem.Controllers
@@ -28,9 +31,12 @@ namespace SchoolManagementSystem.Controllers
         public ActionResult MyBookings() // for student
         {
             var currUser = (UserModel)System.Web.HttpContext.Current.Session["user"];
-            var bookings = repository.Bookings.Where(b => b.Student.PIN == currUser.Login);
+            var booking = db.Bookings.AsNoTracking().Where(b => b.BookedStudent.PIN == currUser.Login);
+            if (booking == null)
+            {
 
-            return View(bookings.ToList());
+            }
+            return View(booking.ToList());
         }
         
         public ActionResult Details(int id)
@@ -59,12 +65,18 @@ namespace SchoolManagementSystem.Controllers
         }
         
         [HttpPost]
-        public ActionResult Create(Booking booking)
+        public ActionResult Create(int? id, Booking booking)
         {
+            var currUser = (UserModel)System.Web.HttpContext.Current.Session["user"];
+            booking.BookedEvent = db.Event.AsNoTracking().FirstOrDefault(e => e.Id == id);
+            booking.BookedStudent = db.Students.AsNoTracking().FirstOrDefault(s => s.PIN == currUser.Login);
+            booking.Status = "Booked";
+
             if (ModelState.IsValid)
             {
                 db.Bookings.Add(booking);
                 db.SaveChanges();
+                SendEmail(booking);
                 return RedirectToAction("Index");
             }
             return View(booking);
@@ -114,6 +126,38 @@ namespace SchoolManagementSystem.Controllers
             db.Bookings.Remove(booking);
             db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        public void SendEmail(Booking model)
+        {
+            var boddy = new StringBuilder();
+            
+            boddy.Append("Dear: " + model.UserName + ","
+                + "<br/>These are your booking details: "
+                + "<br/>Event: " + model.BookedEvent.Name
+                + "<br/>Date: " + model.Date.ToShortDateString()
+                + "<br/>Time: " + model.BookTime.ToShortTimeString()
+                + "<br/>Venue: " + model.BookedEvent.Venue
+                + "<br"
+                + "<br/>We miss you already, visit us again :) ");
+
+            string bodyFor = boddy.ToString();
+            string subjectFor = "Booking";
+
+            string toFor = model.Email;
+            var mail = new MailAddress("21226357@dut4life.ac.za", "Matric Excemption Centre");
+            WebMail.SmtpServer = "pod51014.outlook.com";
+            WebMail.SmtpPort = 587;
+            WebMail.UserName = "21226357@dut4life.ac.za";
+            WebMail.Password = "Dut930623";
+            WebMail.From = mail.ToString();
+            WebMail.EnableSsl = true;
+
+            try { WebMail.Send(toFor, subjectFor, bodyFor); }
+            catch
+            {
+                // ignored
+            }
         }
 
         private ActionResult CheckUserRights()
