f7736bf628fc942df9976d764ffafee7cf0fa0ff
Simphiwe <Simphiwe@SOTOBE>
2016-10-23 08:36:48 +0200

    more stuff for quiz

--- SchoolManagement-master/SchoolManagementSystem/Controllers/QuizController.cs
+++ SchoolManagement-master/SchoolManagementSystem/Controllers/QuizController.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace ShaleCo.OnlineQuiz.Web.Controllers
+namespace SchoolManagementSystem.Controllers
 {
     
     public class QuizController : Controller
